2f135c52c3429162f4dc0b2d8b9ba971d995d67b
Kaue Silveira <silveira.kaue@gmail.com>
2010-06-01 18:31:29 -0300

    quase lendo o tabuleiro

---
+++ Makefile.spec
@@ -0,0 +1,28 @@
+# Default Makefile.spec
+
+#Additional include paths
+INCLUDE +=
+
+#Additional C flags
+CFLAGS += -Wall
+
+#Additional C++ flags
+CPPFLAGS +=
+
+#List of libraries to link with
+LINKWITH +=
+
+#Additional Linker flags
+LDFLAGS +=
+
+#Name of the executable to create
+EXECUTABLE = solver
+
+#Name of the static library to create
+ARCHIVE =
+
+#Name of the dynamic library to create
+DYNLIB =
+
+#Other directories to update before compiling
+SUBDIR =
--- main.cpp
+++ main.cpp
@@ -1,40 +1,84 @@
 #include <iostream>
 
-void print();
+using namespace std;
+
+#define MAX_SIZE 10
+
+static const char input_codes[] = ".*-#+!@"
+
+typedef enum eCode {
+	EMPTY,
+	PKB,
+	PKB_OVER_TARGET,
+	WALL,
+	BOX,
+	BOX_OVER_TARGET,
+	TARGET,
+	NCODES
+} Code;
+
+#define PUKOBAN 1
 
 class Board
 {
 public:
 	void read();
 	void print();
+	static int meaning(char c);
 
 private:
 	int m, n;
+	int board[MAX_SIZE][MAX_SIZE];
 };
 
-int main(int argc, char **argv)
-{
-	int m, n;
-	if(argc != 2) {
-		printf("Usage: %s <file_name>\n", argv[0]);
-		exit(1);
-	}
-	freopen(stdin, argv[1]);
-	int i = 0;
-	while(cin >> line) {
-		n = line.size();
-		for(j = 0; j < line.size(); j++) {
-			tab[i][j] = meaning[line[i]];
-		}
-	}
-	int m = i;
-	
-	print();
-	return 0;
-}
 
-void print()
+void Board::print()
 {
 	int i, j;
 	for(i = 0; i < m; i++) {
 		for(j = 0; j < n; j++)
+			printf("%i ", board[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void Board::read()
+{
+	string line;
+	int i = 0, j;
+	while(cin >> line) {
+		n = line.size();
+		cout << i << ' ' << line << endl;
+		for(j = 0; j < (int) line.size(); j++) {
+			board[i][j] = meaning(line[j]);
+		}
+		i++;
+	}
+	m = i;
+}
+
+int Board::meaning(char c)
+{
+	switch(c) {
+		case PUKOBAN_CHAR:
+			return PUKOBAN;
+		default:
+			return 0;
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	Board b;
+	if(argc != 2) {
+		printf("Usage: %s <file_name>\n", argv[0]);
+		exit(1);
+	}
+	freopen(argv[1], "r", stdin);
+	b.read();
+	b.print();
+	return 0;
+}
+
