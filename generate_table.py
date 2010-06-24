42909734b45f7e3bd93ac07e3bece16962a634fc
bjurkovski <bjurkovski@gmail.com>
2010-06-24 20:58:31 -0300

    script para criar uma tabela no formato csv

---
+++ generate_table.py
@@ -0,0 +1,25 @@
+import sys
+import dircache
+
+if(len(sys.argv) < 3):
+	print "Usage: python generate_table.py solutionsFolder outputTable"
+else:
+	output = sys.argv[2]
+	out = open(output, 'w')
+	out.write("Instancia,Total Branching,Average Branching, Min Branching, Max Branching,States Visited,Solution Length\n")
+
+	dirpath = sys.argv[1]
+	files = dircache.listdir(dirpath)
+	for filename in files:
+		f = open(dirpath+"/"+filename)
+		lines = f.readlines();
+	
+		out.write(filename)
+		for l in lines:
+			words = l.split()
+			out.write(",")
+			out.write(words[0])
+		out.write("\n")
+
+		f.close()
+	out.close()
